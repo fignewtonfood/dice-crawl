c44b7b3ceb8c3935c7a0f40323d084a74dacc977
Tim White <fignewtonfood@gmail.com>
2015-09-26 12:25:50 -0700

    add home/start page

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 var diceCrawl = angular.module('diceCrawl', ['ui.router']);
+
+diceCrawl.config(function($stateProvider, $urlRouterProvider){
+    $stateProvider.state('home', {
+        url: "",
+        templateUrl: "partials/home.html"
+    });
+
+    $stateProvider.state('board', {
+        url: "/board",
+        templateUrl: "partials/board.html",
+        // controller: "StudentsCtrl"
+    });
+});
