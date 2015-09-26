6df4d2856e02d8c89776fc69f79f6079048e1720
Tim White <fignewtonfood@gmail.com>
2015-09-26 14:34:54 -0700

    two tiles show

--- services/TilesFactory.js
+++ services/TilesFactory.js
@@ -1,14 +1,17 @@
 diceCrawl.factory('TilesFactory', function TilesFactory(){
     var factory = {};
-    factory.tiles = [{ image: "img/img1.jpg" }];
+    factory.tiles = [
+        { type: 1, diceNumber: 1, diceOne: 6, image: "img/img1.jpg" },
+        { type: 1, diceNumber: 2, diceOne: 5, diceTwo: 5, image: "img/img2.jpg" }];
 
     // factory.addTile = function() {
         // factory.tiles.push({ type: factory.tileType, diceNumber: factory.tileDiceNumber, diceOne: factory.tileDiceOne, image: "img/img1.jpg", id: factory.tiles.length + 1 });
-        // factory.tiles.push({ image: "img/img1.jpg" });
+        // factory.tiles.push();
         // factory.tileType = null;
         // factory.tileDiceNumber = null;
         // factory.tileDiceOne = null;
         // factory.tileImage = null;
     // };
+
     return factory;
 });
