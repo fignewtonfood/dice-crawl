37ecce0876980759f58d2593e54f292a38a45650
Mike <student@epicodus.com>
2015-10-01 14:59:59 -0700

    shuffle tiles

--- controllers/TilesController.js
+++ controllers/TilesController.js
@@ -1,7 +1,14 @@
 diceCrawl.controller('TilesCtrl', function TilesCtrl($scope, TilesFactory){
-    $scope.tiles = TilesFactory.tiles;
     $scope.TilesFactory = TilesFactory;
     $scope.getTimes=function(n){
         return new Array(n);
     };
+    $scope.shuffle = function(o){
+      for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+      return o;
+    }
+    $scope.tiles = $scope.shuffle(TilesFactory.tiles);
+    console.log($scope.tiles);
+
+
 });
