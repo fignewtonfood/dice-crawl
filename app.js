17c1174e250053da9dcc539d56369d41786c32ab
mike <student@epicodus.com>
2015-10-07 15:45:46 -0700

    stuff

--- app.js
+++ app.js
@@ -39,32 +39,6 @@ diceCrawl.config(function($stateProvider, $urlRouterProvider){
             }
         }
     });
-
-    $stateProvider.state('home.board', {
-        url: '',
-        views: {
-            'dice1':{
-                templateUrl: 'partials/dice1.html',
-                controller: "TilesCtrl"
-            },
-            'dice2':{
-                templateUrl: 'partials/dice2.html',
-                controller: "TilesCtrl"
-            },
-            'dice3':{
-                templateUrl: 'partials/dice3.html',
-                controller: "TilesCtrl"
-            },
-            'dice4':{
-                templateUrl: 'partials/dice4.html',
-                controller: "TilesCtrl"
-            },
-            'dice5':{
-                templateUrl: 'partials/dice5.html',
-                controller: "TilesCtrl"
-            },
-        }
-    });
 });
 
     //     url: "",
