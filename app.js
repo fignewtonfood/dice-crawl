6539ebb03972602b63a7900d236441847065871c
mike <student@epicodus.com>
2015-10-07 13:55:10 -0700

    add dice divs

--- app.js
+++ app.js
@@ -3,8 +3,28 @@ var diceCrawl = angular.module('diceCrawl', ['ui.router']);
 diceCrawl.config(function($stateProvider, $urlRouterProvider){
     $stateProvider.state('home', {
         url: "",
-        templateUrl: "partials/home.html",
-        controller: "PlayersCtrl"
+        views:{
+            'dice1':{
+                templateUrl: 'partials/dice1.html',
+                controller: "TilesCtrl"
+            },
+            'dice2':{
+                templateUrl: 'partials/dice2.html',
+                controller: "TilesCtrl"
+            },
+            'dice3':{
+                templateUrl: 'partials/dice3.html',
+                controller: "TilesCtrl"
+            },
+            'dice4':{
+                templateUrl: 'partials/dice4.html',
+                controller: "TilesCtrl"
+            },
+            'dice5':{
+                templateUrl: 'partials/dice5.html',
+                controller: "TilesCtrl"
+            },
+        },
     });
     $stateProvider.state('board', {
         url: "/board",
